f2bf4db1eef5dd1aa1d03868836b2190bdf41e44
Luiz Amancio <luizfelipeamancio@gmail.com>
2023-11-09 12:58:35 -0300

    correçãoo de dependencias

--- Front_End/comanda-digital/src/app/app-routing.module.ts
+++ Front_End/comanda-digital/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import {LoginComponent} from './pages/login/login.component';
-import {CardapioComponent} from './pages/cardapio/cardapio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CardapioComponent } from './pages/cardapio/cardapio.component';
 import { ComandaComponent } from './pages/comanda/comanda.component';
 
 const routes: Routes = [
--- Front_End/comanda-digital/src/app/app.module.ts
+++ Front_End/comanda-digital/src/app/app.module.ts
@@ -3,36 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './pages/login/login.component';
+import { ComandaComponent } from './pages/comanda/comanda.component';
 import { CardapioComponent } from './pages/cardapio/cardapio.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
-import { MenuComponent } from './menus/menu/menu.component';
-import { CardsComponent } from './components/cardapio-components/cards/cards.component';
-import { SliderComponent } from './components/cardapio-components/slider/slider.component';
-import { ContentsComponent } from './components/cardapio-components/contents/contents.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ComandaComponent } from './pages/comanda/comanda.component';
-import { ContentsComponentComanda } from './components/comanda-components/contents/contents.component';
-import { TabelaPreparoComponent } from './components/comanda-components/tabela-preparo/tabela-preparo.component';
+import { CardsComponent } from './components/cardapio-components/cards/cards.component';
+import { ContentsComponent } from './components/cardapio-components/contents/contents.component';
+import { SliderComponent } from './components/cardapio-components/slider/slider.component';
 import { TabelaComandaComponent } from './components/comanda-components/tabela-comanda/tabela-comanda.component';
-import { MenuPerfilComponent } from './menus/menu-perfil/menu-perfil.component';
+import { TabelaPreparoComponent } from './components/comanda-components/tabela-preparo/tabela-preparo.component';
+import { MenuComponent } from './components/menus/menu/menu.component';
+import { MenuPerfilComponent } from './components/menus/menu-perfil/menu-perfil.component';
+import { ContentsComponentComanda } from './components/comanda-components/contents/contents.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    ComandaComponent,
     CardapioComponent,
     FormLoginComponent,
-    MenuComponent,
-    CardsComponent,
-    SliderComponent,
-    ContentsComponent,
     FooterComponent,
-    ComandaComponent,
-    ContentsComponentComanda,
-    TabelaPreparoComponent,
+    CardsComponent,
+    ContentsComponent,
+    SliderComponent,
     TabelaComandaComponent,
+    TabelaPreparoComponent,
+    ContentsComponentComanda,
+    MenuComponent,
     MenuPerfilComponent
   ],
   imports: [
